a47db80cae2cecf3263df9e38c635316bd53b822
ɮɨӄǟֆɦ ʍǟɨȶʏ <118190786+BikashTG@users.noreply.github.com>
2022-11-23 07:52:04 +0530

    Update imdb.py

--- plugins/imdb.py
+++ plugins/imdb.py
@@ -1,1 +1,11 @@
-
+import os
+from pyrogram import Client, filters
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
+from caption import IMDB_CAPTION
+from utils import extract_user, get_file_id, get_poster
+import time
+from datetime import datetime
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
