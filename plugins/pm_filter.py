45d6fe8e57c582d5bf206a0764a81c29b5010719
ɮɨӄǟֆɦ ʍǟɨȶʏ <118190786+BikashTG@users.noreply.github.com>
2022-11-27 22:28:28 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -33,8 +33,6 @@ SPELL_CHECK = {}
 
 @Client.on_message(filters.group & filters.text & filters.chat(-1001510283128) & filters.incoming)
 async def give_filter(client, message):
-    k = await manual_filters(client, message)
-    if k == False:
         await auto_filter(client, message)
 
 @Client.on_callback_query(filters.regex(r"^next"))
