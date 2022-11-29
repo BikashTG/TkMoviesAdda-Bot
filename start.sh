882af85e36807e58b23e57e980bc296773497350
ɮɨӄǟֆɦ ʍǟɨȶʏ <118190786+BikashTG@users.noreply.github.com>
2022-11-29 12:43:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/BikashTG/UniqueMovie-Bot.git /UniqueMovie-Bot
-cd /UniqueMovie-Bot
+git clone -b master https://github.com/BikashTG/TkMoviesAdda-Bot.git /TkMoviesAdda-Bot
+cd /TkMoviesAdda-Bot
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot Please Wait..."
